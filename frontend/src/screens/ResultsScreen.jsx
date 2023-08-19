c230797ce84fa5d14056f3591b34d771e2cb5e8a
AAZZAZRON <aaronzhu3.1415@gmail.com>
2023-08-19 19:38:48 -0400

    connect upload and results

--- frontend/src/screens/ResultsScreen.jsx
+++ frontend/src/screens/ResultsScreen.jsx
@@ -2,25 +2,36 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import NavBar from "../components/NavBar";
 import '../styles/ResultsScreen.css';
+import { useLocation } from "react-router";
 
 export default function ResultsScreen() {
     const [manufacturers, setManufacturers] = useState(null);
     const [rawMaterials, setRawMaterials] = useState(null);
+    const [device, setDevice] = useState(null);
     const [loading, isLoading] = useState(false);
+    const location = useLocation();
+    const acceptable_devices = ['iphone', 'android']
+
 
     useEffect(() => {
-        axios.get("http://localhost:5000/api/iphone")
-        .then((response) => {
-            console.log(response);
-            console.log(response.data.manufacturers);
-            console.log(response.data.raw_materials);
+        const fetchData = async () => {
+            console.log(location.state);
+            setDevice(location.state);
+
+            if (!acceptable_devices.includes(location.state)) {
+                setDevice("Invalid Device");
+                isLoading(true);
+                return;
+            }
+        
+            const response = await axios.get("http://localhost:5000/api/iphone");
             setManufacturers(response.data.manufacturers);
             setRawMaterials(response.data.raw_materials);
             isLoading(true);
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+        };
+
+        
+        fetchData();
     }, []);
 
 
@@ -30,7 +41,7 @@ export default function ResultsScreen() {
         <NavBar />
             {loading ? (
                 <div>
-                    <h1>Results</h1>
+                    <h1>Results {device}</h1>
                     { manufacturers && 
                         Object.keys(manufacturers).map((key) => {
                             console.log(key);
