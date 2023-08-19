bcd58f8781c7839fc73aec06c8b668843331e1bb
AAZZAZRON <aaronzhu3.1415@gmail.com>
2023-08-19 01:37:01 -0400

    set up functions fro cohere calls

---
+++ backend/get_cohere_data.py
@@ -0,0 +1,30 @@
+import os
+from langchain.llms import Cohere
+from langchain import PromptTemplate, LLMChain
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+COHERE_API_KEY = os.environ.get("COHERE_API_KEY")
+
+
+def get_cohere_data(question):
+    template = "Question: {question}"
+
+    prompt = PromptTemplate(template=template, input_variables=["question"])
+
+    llm = Cohere(cohere_api_key=COHERE_API_KEY, model="command-nightly")
+    llm_chain = LLMChain(prompt=prompt, llm=llm)
+
+    answer = llm_chain.run(question)
+    return answer
+
+def get_raw_materials():
+    question = ''
+    return get_cohere_data(question)
+
+def get_manufacturers():
+    question = ''
+    return get_cohere_data(question)
+
