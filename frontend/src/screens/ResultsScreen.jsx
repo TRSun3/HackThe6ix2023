05e16dc642ad0929f4615b4a38fe43e21fa1f45f
Pancreas <108845209+Pancraes@users.noreply.github.com>
2023-08-19 21:10:58 -0400

    changed results background

--- frontend/src/screens/ResultsScreen.jsx
+++ frontend/src/screens/ResultsScreen.jsx
@@ -4,6 +4,7 @@ import NavBar from "../components/NavBar";
 import '../styles/ResultsScreen.css';
 import bigTitle from "../assets/temporary shit logo27.png";
 import { useLocation } from "react-router";
+import bg from "../assets/bigglobe.gif";
 
 export default function ResultsScreen() {
     const [manufacturers, setManufacturers] = useState(null);
@@ -37,11 +38,21 @@ export default function ResultsScreen() {
         fetchData();
     }, [device]);
 
+    const bgStyles = {
+        backgroundImage: `url(${bg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        height: "100vh",
+        width: "100vw",
+        top:0,
+        display: 'flex',
+      };
+
 
     return (
         <>
-        <div className="background">
-        <NavBar />
+        <div className="background" style={bgStyles}>
             {loading ? (
                 <div>
                     <div className="top">
